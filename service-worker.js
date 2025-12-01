1514dc5de5b00cede40859084371a4bc3037186d
Kaleab Solomon <kaleabsolomon59@gmail.com>
2025-12-02 02:41:49 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ const urlsToCache = [
   './manifest.json'
 ];
 
-// Install
+// Install service worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -16,7 +16,7 @@ self.addEventListener('install', event => {
   );
 });
 
-// Fetch
+// Fetch event
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
